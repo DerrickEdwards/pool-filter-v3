a8bdc955eac598db65be02466fdb4c4eddd1ae24
DerrickEdwards <derrickedwards@ags-us.com>
2016-12-04 19:12:44 -0600

    updates to UI

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-function helloMessage(){
+// =============================================================================
+function filterPower(){
 
-   document.getElementById( "messageDisplay" ).innerHTML = "Hello From JavaScript"
 
-}; // end hello()
+} // end filterPowerStatus()
 
 
 // =============================================================================
+function toggleFilterPower(){
+
+
+} // end filterPowerStatus()
+
+
+
+// =============================================================================
+function currentWeather(){ 
 
 // wunderground api key 94d35539887201df
-
-function getCurrentWeather(){ 
 	$.ajax({
 		url : "http://api.wunderground.com/api/94d35539887201df/geolookup/conditions/q/WI/River_Falls.json",
 		dataType : "jsonp",
@@ -18,7 +25,7 @@ function getCurrentWeather(){
 			var temp_f 		= parsed_json['current_observation']['temp_f'];
 			var icon_url 	= parsed_json['current_observation']['icon_url'];
 			$( "#weatherIcon" ).css( "background-image", "url(" + icon_url + ")" );			
-			$( "#messageDisplay" ).text( temp_f );
+			$( "#temperature" ).text( temp_f );
 		}
 	});		
-} // end getCurrentWeather()
+} // end currentWeather()
