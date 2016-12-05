edf10e4f0481574a4584c09426fde54ba59fae65
DerrickEdwards <derrickedwards@ags-us.com>
2016-12-05 06:21:16 -0600

    prepping for move to rpi

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require( "express" );
 var app     = express();
+var path    = require( "path" );
+
+var Gpio = require( "onoff" ).Gpio; // https://www.npmjs.com/package/onoff
+var led = new Gpio( 12,'low');
+
 
 // This is only serving static pages
 // This is the worlds most simplistic security protocol
@@ -11,10 +16,50 @@ var app     = express();
 // @todo: add a time in if there is a bad request so that the server cannot be made to respond quickly to a brute force attack
 
 
+// allow CORS so a local browser working from "localhost" can make calls to the server's ip address
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // use "/abc/<filename>" as a virtual path to "/<filename>"
-app.use( "/abc", express.static("./"));
+//app.use( "/abc", express.static("./"));
+
+app.get( "/", function(req,res){ res.sendFile( path.join( __dirname, "index.html" ) ) })
+app.get( "/style.css", function(req,res){ res.sendFile( path.join( __dirname, "style.css" ) ) })
+app.get( "/script.js", function(req,res){ res.sendFile( path.join( __dirname, "script.js" ) ) })
+
+app.get('/ledToggle', ledToggle );
+app.get('/ledOn', ledOn );
+app.get('/ledOff', ledOff );
 
 app.listen( 80, console.log( "[ Express Server ] Listening on port 80" ));
 
 
- 
+
+// ===========================================================
+function ledToggle( req, res ){
+    res.sendStatus(200);
+    console.log( "toggleLed() called");
+    if ( led.readSync() === 1 ){
+        led.writeSync(0); 
+    } else {
+        led.writeSync(1);
+    }
+}; // end toggleLed()
+
+// ===========================================================
+function ledOn( req, res ){
+    res.sendStatus(200);
+    console.log( "ledOn() called");
+    led.writeSync(1);
+}; // end toggleLed()
+
+// ===========================================================
+function ledOff( req, res ){
+    res.sendStatus(200);
+    console.log( "ledOff() called");
+    led.writeSync(0);
+}; // end toggleLed()
